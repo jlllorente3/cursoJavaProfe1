b69285380b3051913bad84a8ba56e3154173b1ae
josel <josel@JOSE-MSI>
2024-09-12 13:49:53 +0200

    Bucles - While y doWhile

---
+++ src/es/cursojava/inicio/bucles/BucleDoWhile.java
@@ -0,0 +1,15 @@
+package es.cursojava.inicio.bucles;
+
+public class BucleDoWhile {
+
+	public static void main(String[] args) {
+		int numero = 11;
+		do {
+			System.out.println("Hola "+numero);
+			
+		}while(numero<10);
+		
+
+	}
+
+}
---
+++ src/es/cursojava/inicio/bucles/BucleWhile.java
@@ -0,0 +1,16 @@
+package es.cursojava.inicio.bucles;
+
+public class BucleWhile {
+
+	public static void main(String[] args) {
+		
+		int numero = 3;
+		while (2>numero) {
+			System.out.println("Hola" + numero);
+			
+			numero++;
+		}
+		System.out.println("FIN");
+	}
+
+}
