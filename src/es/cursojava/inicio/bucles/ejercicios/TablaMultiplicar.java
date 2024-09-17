89823396db6f511211d3e059f29454e75c92f429
josel <josel@JOSE-MSI>
2024-09-17 09:42:31 +0200

    Ejercicio Tabla Multiplicar

---
+++ src/es/cursojava/inicio/bucles/ejercicios/TablaMultiplicar.java
@@ -0,0 +1,14 @@
+package es.cursojava.inicio.bucles.ejercicios;
+
+public class TablaMultiplicar {
+	//Pintar la tabla de multiplicar de un numero
+	public static void main(String[] args) {
+		
+		int numero = 6;
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i + "x" + numero +"="+ (i*numero));
+		}
+		
+	}
+
+}
